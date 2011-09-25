d3f0bb6cac9b3e386ef4c615c2dbf54be619e758
Scyptnex <scyptnex@Therauvra.(none)>
2011-09-26 08:46:06 +1000

    ran some experiments

---
+++ runexp.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+if [ "$1lol" = "lol" ]
+then
+	echo "need a folder"
+	exit
+fi
+
+for fn in `ls -rS $1`
+do
+	out=`echo $fn | sed 's/exp$/opt/g'`
+	log=`echo $fn | sed 's/exp$/log/g'`
+	if [ -f $out ]
+	then
+		echo "skipping $out"
+	else
+		echo "running $out"
+		glpsol -m ~/computing/sdfSimulator/cplex/ip.mod -d $1/$fn -o $1/$out > $log
+	fi
+done
